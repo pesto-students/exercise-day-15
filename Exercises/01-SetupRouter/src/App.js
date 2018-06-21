71c3beb81d1e31be3d9f414916a282f4ba76d1fd
Nikhil Arora <narora200@gmail.com>
2018-06-21 23:08:13 +0530

    WIP: Router setup

--- Exercises/01-SetupRouter/src/App.js
+++ Exercises/01-SetupRouter/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
+import { Route, Link } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -12,9 +14,20 @@ class App extends Component {
     return (
       <Fragment>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Link to="/home">
+          <Home />
+        </Link>
+        <Link to="/about">
+          <About />
+        </Link>
+        <Link to="/contact">
+          <Contact />
+        </Link>
+
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+
       </Fragment>
     );
   }
