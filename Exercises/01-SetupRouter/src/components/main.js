91a445c2b864f7e4dd88c7aa0b9bf74fa832593e
rajul <gamerrajul@gmail.com>
2018-06-21 23:39:51 +0530

    WIP

--- Exercises/01-SetupRouter/src/components/main.js
+++ Exercises/01-SetupRouter/src/components/main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import noMatch from './noMatch';
 
 const main = () => (
   <main>
@@ -10,6 +11,7 @@ const main = () => (
       <Route exact path="/home" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/contact" component={Contact} />
+      <Route path="*" component={noMatch} />
     </Switch>
   </main>
 );
