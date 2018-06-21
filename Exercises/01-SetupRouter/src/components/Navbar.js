4d9f8733ab840ff47446bd6fd166312685c8a57a
Kundan Singh <kundan.singh979@gmail.com>
2018-06-21 23:09:28 +0530

    WIP: React Router Solution

--- Exercises/01-SetupRouter/src/components/Navbar.js
+++ Exercises/01-SetupRouter/src/components/Navbar.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Home from './../components/Home';
+import About from './../components/About';
+import Contact from './../components/Contact';
+import NotFound from './../components/NotFound';
+
 
 import '../styles/App.css';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
+      <Router>
+        <div>
+          <ul className="nav">
+            <li className="active">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
