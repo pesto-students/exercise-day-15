2095e93beaad26f7882892254a8805e6f8b61ef9
Amal <amaledassery@gmail.com>
2018-06-22 00:02:27 +0530

    navigation

--- Exercises/01-SetupRouter/src/components/Main.js
+++ Exercises/01-SetupRouter/src/components/Main.js
@@ -1,5 +1,5 @@
 import { Switch, Route } from 'react-router-dom';
-import React, { Component } from 'react';
+import React from 'react';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
@@ -7,9 +7,10 @@ import Contact from './Contact';
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="./components/About" component={About} />
-      <Route path="./components/Contact" component={Contact} />
+      <Route exact path="/Home" component={Home} />
+      <Route path=".About" component={About} />
+      <Route path="./Contact" component={Contact} />
     </Switch>
   </main>
 );
+export default Main;
--- Exercises/01-SetupRouter/src/components/Navbar.js
+++ Exercises/01-SetupRouter/src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/App.css';
+
 
 export default class Navbar extends Component {
   render() {
     return (
       <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
+        <li className="active"> <Link to="/home"> Home </Link> </li>
+        <li><Link to="/about"> About </Link></li>
+        <li><Link to="/contact"> Contact </Link></li>
       </ul>
     );
   }
