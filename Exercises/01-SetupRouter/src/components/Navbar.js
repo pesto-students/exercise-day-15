0f698aae844d1ab7d8affd2750bb32c63cee9605
monisahmad <monis.ahmad42@gmail.com>
2018-06-21 23:56:17 +0530

    Monis:Segment-1 Routes

--- Exercises/01-SetupRouter/src/components/Navbar.js
+++ Exercises/01-SetupRouter/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/App.css';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
-      </ul>
+      <div className="nav">
+        <li><Link to="/" className="active">Home</Link></li>
+        <li><Link to="/Contact">Contact</Link></li>
+        <li><Link to="/About">About</Link></li>
+      </div>
     );
   }
 }
