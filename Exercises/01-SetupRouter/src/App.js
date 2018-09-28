d570c84a8e12d3ec5421083af0b24c0d9cdab949
Bharani <bharanidharan.nb@gmail.com>
2018-09-28 23:21:07 +0530

    01-SetupRouter

--- Exercises/01-SetupRouter/src/App.js
+++ Exercises/01-SetupRouter/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 import './styles/App.css';
 
@@ -12,9 +14,14 @@ class App extends Component {
     return (
       <Fragment>
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <Switch>
+          <Redirect from="/" to="/home" exact />
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
+
       </Fragment>
     );
   }
