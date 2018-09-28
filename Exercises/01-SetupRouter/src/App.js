779cf37d3bed5b8282319480eca4dd887fbad473
vipulrawat <vipulrawat.imi@live.in>
2018-09-28 23:32:21 +0530

    [WIP]

--- Exercises/01-SetupRouter/src/App.js
+++ Exercises/01-SetupRouter/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,15 +7,24 @@ import Contact from './components/Contact';
 
 import './styles/App.css';
 
+const NoMatch = () => <h2>404</h2>;
+
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
-      </Fragment>
+      <Router>
+        <div>
+          <Fragment>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Fragment>
+        </div>
+      </Router>
     );
   }
 }
--- Exercises/01-SetupRouter/src/components/Navbar.js
+++ Exercises/01-SetupRouter/src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import '../styles/App.css';
 
 export default class Navbar extends Component {
   render() {
     return (
       <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
+        <li className="active"><Link to="/">Home</Link></li>
+        <li><Link to="/about">About us</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     );
   }
