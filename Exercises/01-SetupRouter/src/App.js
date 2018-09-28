dc72de2bf8380ac2ce45972d592dbf1416209a4d
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-28 23:18:18 +0530

    Did Exercise 1

--- Exercises/01-SetupRouter/src/App.js
+++ Exercises/01-SetupRouter/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </Fragment>
+      </Router>
     );
   }
 }
